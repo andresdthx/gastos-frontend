4daa96c51213abea7f4ccef6fcf48a4eda297143
Andres Canchila <andresfelipecx@gmail.com>
2021-09-09 22:22:01 -0500

    diseño tabla pc and drawers

--- src/service-worker.js
+++ src/service-worker.js
@@ -75,7 +75,7 @@ self.addEventListener('push', e => {
 
   self.registration.showNotification(data.title, {
     body: data.message,
-    icon: 'https://pixabay.com/vectors/bell-notification-communication-1096280/'
-    // icon: 'images/background/sidebar.jpg'
+    // icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Archlinux-icon-crystal-64.svg/1200px-Archlinux-icon-crystal-64.svg.png',
+    icon: 'logoIcon.png',
   });
 })
